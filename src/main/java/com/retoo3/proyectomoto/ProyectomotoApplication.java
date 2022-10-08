f5518b61b7f98efd89aba585d875b24f96296163
sealmo47 <sebastian.morales.mt@usa.edu.co>
2022-10-07 22:44:20 -0500

    Se crea Proyecto en spring
    Se crea Proyecto en spring

---
+++ src/main/java/com/retoo3/proyectomoto/ProyectomotoApplication.java
@@ -0,0 +1,13 @@
+package com.retoo3.proyectomoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectomotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retoo3/proyectomoto/ProyectomotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retoo3.proyectomoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectomotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
